37f097a957ffb364d06d367cfdc8f37bcb91cbee
Md. Inzamam-Ul Haque Sobuz <48618774+inzamam-inz@users.noreply.github.com>
2021-07-13 16:09:09 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function moreDesignation() { 
+    document.getElementById("hideDesignation").innerHTML = "Expert coder in Codeforces<br>5 star coder in Codechef<br>Knight coder in Leetcode";
+}
+
+function moreDescribe() { 
+    document.getElementById("hideDescribe").innerHTML = " I am interested in competitive programming.";
+}
