c7c9049d71a6bb25f920a2f45ce2925ea4d57f93
Md. Inzamam-Ul Haque Sobuz <48618774+inzamam-inz@users.noreply.github.com>
2022-12-11 21:26:46 +0600

    Info updated and CV added

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ function moreDesignation() {
 function moreDescribe() { 
     document.getElementById("hideDescribe").innerHTML = " I am interested in competitive programming.";
 }
+
+function downloadPDF() {
+    const url = '';
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'my-pdf-file.pdf';
+    link.click();
+}
